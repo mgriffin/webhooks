cbc3df247f3349d5c30288973c5c16ca9a502d36
Mike Griffin <mgriffin@github.com>
2020-06-19 23:57:45 +0100

    Adds more boiler plate
    This time to validate the payload, taken from
    https://developer.github.com/enterprise/webhooks/securing/#validating-payloads-from-github

--- server.rb
+++ server.rb
@@ -5,3 +5,16 @@ post '/payload' do
   push = JSON.parse(request.body.read)
   puts "I got some JSON: #{push.inspect}"
 end
+
+post '/payload-with-token' do
+  request.body.rewind
+  payload_body = request.body.read
+  verify_signature(payload_body)
+  push = JSON.parse(params[:payload])
+  "I got some JSON: #{push.inspect}"
+end
+
+def verify_signature(payload_body)
+  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+end
